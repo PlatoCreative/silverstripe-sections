3ddc8115fd7ec3f1e70af608ebd218a9ae29953d
Gorrie <gorriecoe@gmail.com>
2016-02-02 21:15:23 +1300

    Gallery section no longer extends ImageSection

--- code/sections/GallerySection.php
+++ code/sections/GallerySection.php
@@ -1,5 +1,36 @@
 <?php
-class GallerySection extends ImageSection
+class GallerySection extends Section
 {
-    
+    /**
+    * Many_many relationship
+    * @return array
+    */
+    private static $many_many = array(
+        'Images' => 'Image'
+    );
+
+    private static $many_many_extraFields = array(
+        'Images' => array(
+            'SortOrder' => 'Int'
+        )
+    );
+
+    /**
+     * CMS Fields
+     * @return array
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldsToTab(
+            "Root.Main",
+            array(
+                SortableUploadField::create(
+                    'Images',
+                    'Current Image(s)'
+                )
+            )
+        );
+        return $fields;
+    }
 }
