efc612c7547acbd5685b0bede6027331242e8e5e
gorrie <gorriecoe@gmail.com>
2016-02-28 23:33:11 +1300

    Added js to auto complete admintitle from menutitle

--- js/CMSMain.SectionForm.js
+++ js/CMSMain.SectionForm.js
@@ -1,5 +1,5 @@
 /**
- * File: CMSMain.EditForm.js
+ * File: CMSMain.SectionForm.js
  */
 (function($) {
 	$.entwine('ss', function($){
@@ -8,81 +8,30 @@
 		 *
 		 * Input validation on the Title field
 		 */
-		$('.cms-edit-form input[name=Title]').entwine({
+
+		$('.cms-edit-form input[name=MenuTitle]').entwine({
 			// Constructor: onmatch
 			onmatch : function() {
 				var self = this;
-
 				self.data('OrigVal', self.val());
 
 				var form = self.closest('form');
-				var urlSegmentInput = $('input:text[name=URLSegment]', form);
-				var liveLinkInput = $('input[name=LiveLink]', form);
+				var AdminTitle = $('input:text[name=AdminTitle]', form);
+                var MenuTitle = $('input:text[name=MenuTitle]', form);
 
-				if (urlSegmentInput.length > 0) {
+				if (MenuTitle.length > 0) {
 					self._addActions();
 					this.bind('change', function(e) {
 						var origTitle = self.data('OrigVal');
 						var title = self.val();
 						self.data('OrigVal', title);
-
-						// Criteria for defining a "new" page
-						if (
-							urlSegmentInput.val().indexOf(urlSegmentInput.data('defaultUrl')) === 0
-							&& liveLinkInput.val() == ''
-						) {
-							self.updateURLSegment(title);
-						} else {
-							$('.update', self.parent()).show();
-						}
-
 						self.updateRelatedFields(title, origTitle);
+                        if(AdminTitle.val() == origTitle){
+                            AdminTitle.val(title);
+                        }
 					});
 				}
-
-				this._super();
-			},
-			onunmatch: function() {
-				this._super();
-			},
-
-			/**
-			 * Function: updateRelatedFields
-			 *
-			 * Update the related fields if appropriate
-			 * (String) title The new title
-			 * (Stirng) origTitle The original title
-			 */
-			updateRelatedFields: function(title, origTitle) {
-				// Update these fields only if their value was originally the same as the title
-				this.parents('form').find('input[name=MenuTitle]').each(function() {
-					var $this = $(this);
-					if($this.val() == origTitle) {
-						$this.val(title);
-						// Onchange bubbling didn't work in IE8, so .trigger('change') couldn't be used
-						if($this.updatedRelatedFields) $this.updatedRelatedFields();
-					}
-				});
-			},
-
-			/**
-			 * Function: updateURLSegment
-			 *
-			 * Update the URLSegment
-			 * (String) title
-			 */
-			updateURLSegment: function(title) {
-				var urlSegmentInput = $('input:text[name=URLSegment]', this.closest('form'));
-				var urlSegmentField = urlSegmentInput.closest('.field.urlsegment');
-				var updateURLFromTitle = $('.update', this.parent());
-				urlSegmentField.update(title);
-				if (updateURLFromTitle.is(':visible')) {
-					updateURLFromTitle.hide();
-				}
-			},
-
-
-
+			}
 		});
 	});
 }(jQuery));
