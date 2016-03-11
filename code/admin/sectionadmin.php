3c00b2c800a93979eca042c51fde9903d507d93d
Gorrie <gorriecoe@gmail.com>
2016-03-11 14:03:58 +1300

    Added a model admin to allow editing of all sections on the site.

---
+++ code/admin/sectionadmin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Description
+ *
+ * @package silverstripe
+ * @subpackage mysite
+ */
+class SectionAdmin extends ModelAdmin
+{
+    /**
+     * Managed data objects for CMS
+     * @var array
+     */
+    private static $managed_models = array(
+        'Section'
+    );
+
+    /**
+     * URL Path for CMS
+     * @var string
+     */
+    private static $url_segment = 'sections';
+
+    /**
+     * Menu title for CMS
+     * @var string
+     */
+    private static $menu_title = 'All Sections';
+
+
+}
